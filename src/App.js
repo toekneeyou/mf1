b980c5428a283c1ad9a806674defad39378fd83c
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-08-05 15:12:00 -0700

    button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
 
-const App = (props) => {
-  const buttonClick = () => {
-    const onClick = props.onClick;
-    if (onClick) {
-      onClick();
-    } else {
-      console.log("button is clicked");
-    }
-  };
-  return <div style={{ padding: "50px 12px", display: "flex" }}>I'm from a different APP</div>;
+const App = () => {
+  return <button>Button from app 1</button>;
 };
 
 export default App;
